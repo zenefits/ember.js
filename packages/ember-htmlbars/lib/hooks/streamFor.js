7525900184758d5a4ae33d67c1ffc34fea258d7f
Erik Bryn <erik.bryn@gmail.com>
2014-06-25 17:13:40 -0700

    Port `each` helper

--- packages/ember-htmlbars/lib/hooks/streamFor.js
+++ packages/ember-htmlbars/lib/hooks/streamFor.js
@@ -3,6 +3,7 @@ import { get } from "ember-metal/property_get";
 import LazyValue from "bound-templates/lazy-value";
 
 var VIEW_KEYWORD_REGEX = /^view\./;
+var FIRST_KEY = /^([^\.]+)/;
 
 function streamFor(view, path) {
   var streams = view.streams;
@@ -13,14 +14,20 @@ function streamFor(view, path) {
   // Ideally:
   // Ember.addObserver(view, 'context.' + path, this, 'streamPropertyDidChange');
 
+  var keywords = view.templateData && view.templateData.keywords,
+      matches = path.match(FIRST_KEY),
+      firstKey = matches && matches[0];
+
   if (path === '') { // handle {{this}}
     // TODO: possible optimization: reuse the context observer that already exists.
     //       this would require us to return some other type of stream object.
     stream = new EmberObserverLazyValue(view, 'context');
-  } else if (VIEW_KEYWORD_REGEX.test(path)) {
-    stream = new EmberObserverLazyValue(view, path.slice(5));
+  // } else if (VIEW_KEYWORD_REGEX.test(path)) {
+    // stream = new EmberObserverLazyValue(view, path.slice(5));
   // } else if (view.context) {
   //   stream = new EmberObserverLazyValue(view.context, path);
+  } else if (keywords && firstKey in keywords) {
+    stream = new EmberObserverLazyValue(view, 'templateData.keywords.' + path);
   } else {
     stream = new EmberObserverLazyValue(view, 'context.'+path);
   }
--- packages/ember-metal-views/lib/renderer.js
+++ packages/ember-metal-views/lib/renderer.js
@@ -187,7 +187,9 @@ function Renderer_remove(_view, shouldDestroy) {
 
 function Renderer_insertElement(view, parentView, element, index) {
   if (view._morph) {
-    view._morph.update(element);
+    if (element) {
+      view._morph.update(element);
+    }
   } else if (parentView) {
     if (index === -1) {
       view._morph = parentView._childViewsMorph.append(element);
