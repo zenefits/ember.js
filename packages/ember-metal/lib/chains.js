4391ed877758af092161ca8ed573abe7f67f7ad4
Yehuda Katz <wycats@gmail.com>
2015-03-04 12:20:56 -0800

    Revert "WIP perf work"
    This reverts commit f8174038f166c4f3d044673dfc865870fdbd26c1.

--- packages/ember-metal/lib/chains.js
+++ packages/ember-metal/lib/chains.js
@@ -36,7 +36,7 @@ function addChainWatcher(obj, keyName, node) {
   var m = metaFor(obj);
   var nodes = m.chainWatchers;
 
-  if (!m.chainWatchers || !m.hasOwnProperty('chainWatchers')) { // FIXME?!
+  if (!m.hasOwnProperty('chainWatchers')) { // FIXME?!
     nodes = m.chainWatchers = {};
   }
 
@@ -51,7 +51,7 @@ function removeChainWatcher(obj, keyName, node) {
   if (!obj || 'object' !== typeof obj) { return; } // nothing to do
 
   var m = obj['__ember_meta__'];
-  if (m && (!m.chainWatchers || !m.hasOwnProperty('chainWatchers'))) { return; } // nothing to do
+  if (m && !m.hasOwnProperty('chainWatchers')) { return; } // nothing to do
 
   var nodes = m && m.chainWatchers;
 
