c24124b9bee6b57e109ed7a360e7d5b398f79475
Martin Muñoz <im.mmun@gmail.com>
2015-03-13 15:39:11 -0400

    Add support for binding to global paths

--- packages/ember-htmlbars/tests/helpers/bind_attr_test.js
+++ packages/ember-htmlbars/tests/helpers/bind_attr_test.js
@@ -129,7 +129,7 @@ QUnit.test("should be able to bind to globals with {{bind-attr}}", function() {
 
   expectDeprecation(function() {
     runAppend(view);
-  }, /Global lookup of TemplateTests.value from a Handlebars template is deprecated/);
+  }, /Global lookup of TemplateTests from a Handlebars template is deprecated/);
 
   equal(view.$('img').attr('alt'), "Test", "renders initial value");
 });
@@ -413,7 +413,7 @@ QUnit.test("should be able to bind classes to globals with {{bind-attr class}}",
 
   expectDeprecation(function() {
     runAppend(view);
-  }, /Global lookup of TemplateTests.isOpen from a Handlebars template is deprecated/);
+  }, /Global lookup of TemplateTests from a Handlebars template is deprecated/);
 
   ok(view.$('img').hasClass('is-open'), "sets classname to the dasherized value of the global property");
 });
