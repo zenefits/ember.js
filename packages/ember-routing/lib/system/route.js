a92e2bd4c7737700e3767e6687797e5090531fd9
Edward Zhang <ezhang90@gmail.com>
2015-08-24 19:58:35 -0700

    change findModel for Ember Model

--- packages/ember-routing/lib/system/route.js
+++ packages/ember-routing/lib/system/route.js
@@ -1375,13 +1375,7 @@ var Route = EmberObject.extend(ActionHandler, Evented, {
   */
   findModel: function() {
     var store = get(this, 'store');
-    if (typeof DS.Transform !== 'undefined') {
-      return store.find.apply(store, arguments);
-    }
-    var type = arguments[0];
-    var value = arguments[1];
-    var modelClass = this.container.lookupFactory('model:' + type);
-    return modelClass.find(value);
+    return store._findSync.apply(store, arguments);
   },
 
   /**
