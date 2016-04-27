83275d481c6130b3e682d6a03ca1b2d65f1d959c
Quinn Hoyer <qhoyer@zenefits.com>
2016-04-27 11:22:00 -0800

    Revert "Disable Ember version logging by default"
    This reverts commit b977124613435c2605a913250b6ac4a4223e6d46.

--- packages/ember-metal/lib/core.js
+++ packages/ember-metal/lib/core.js
@@ -199,10 +199,10 @@ Ember.SHIM_ES5 = (Ember.ENV.SHIM_ES5 === false) ? false : Ember.EXTEND_PROTOTYPE
 
   @property LOG_VERSION
   @type Boolean
-  @default false
+  @default true
   @public
 */
-Ember.LOG_VERSION = Ember.ENV.LOG_VERSION === true;
+Ember.LOG_VERSION = (Ember.ENV.LOG_VERSION === false) ? false : true;
 
 /**
   An empty function useful for some operations. Always returns `this`.
